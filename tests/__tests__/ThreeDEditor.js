5cc0beeddf9382290b35d3d4e322b7618565c1a9
Mohammad Mohammadi <mohammad@exabyte.io>
2019-02-11 19:41:45 -0800

    Move ThreeDEditor tests to ThreeDEditor

---
+++ tests/__tests__/ThreeDEditor.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {Made} from "../../../made-js/lib/made";
+import {shallow} from 'enzyme';
+
+import {MATERIAL_CONFIG} from "../enums";
+import {ThreeDEditor} from "../../src/components/ThreeDEditor";
+
+test('ThreeDEditor Base Test', async () => {
+    const component = shallow(<ThreeDEditor material={new Made.Material(MATERIAL_CONFIG)}/>);
+    expect(component).toMatchSnapshot();
+});
--- tests/__tests__/wave.js
+++ tests/__tests__/wave.js
@@ -1,10 +1,5 @@
-import React from 'react';
-import {Made} from "made.js";
-import {shallow} from 'enzyme';
-
+import {getWaveInstance} from "../enums";
 import {takeSnapshotAndAssertEquality} from "../utils";
-import {getWaveInstance, MATERIAL_CONFIG} from "../enums";
-import {ThreeDEditor} from "../../src/components/ThreeDEditor";
 
 test('draw silicon', async () => {
     const wave = getWaveInstance();
@@ -15,9 +10,4 @@ test('draw silicon with toggled axes', async () => {
     const wave = getWaveInstance();
     wave.toggleAxes();
     return takeSnapshotAndAssertEquality(wave.renderer.context, "siWithAxes");
-});
-
-test('ThreeDEditor Base Test', async () => {
-    const component = shallow(<ThreeDEditor material={new Made.Material(MATERIAL_CONFIG)}/>);
-    expect(component).toMatchSnapshot();
 });
