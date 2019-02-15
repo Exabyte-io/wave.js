af79d425802ad7193bbb80b2f05b20c6ea0a174d
Mohammad Mohammadi <mohammad@exabyte.io>
2019-02-15 10:23:11 -0800

    Fix imports

--- tests/enums.js
+++ tests/enums.js
@@ -1,3 +1,4 @@
+import fs from "fs";
 import path from "path";
 import {Made} from "made.js";
 
@@ -16,25 +17,9 @@ export const ELEMENT_PROPERTIES = {
     clientHeight: {value: HEIGHT},
 };
 
-import fs from "fs-extra";
-import _ from "underscore";
-import {expect} from "chai";
-
-export function readFile(filePath, coding = 'utf8') {
-    return fs.readFileSync(filePath, coding);
-}
-
-export function readJSONFile(filePath) {
-    return JSON.parse(readFile(filePath));
-}
-
-export function assertDeepAlmostEqual(leftHandOperand, rightHandOperand, excludedKeys = []) {
-    expect(_.omit(leftHandOperand, excludedKeys)).to.be.deep.almost.equal(_.omit(rightHandOperand, excludedKeys));
-}
-
 // default material config
 const materialJsonFilePath = path.resolve(__dirname, "material.json");
-export const MATERIAL_CONFIG = JSON.parse(readFile(fs.readFileSync(materialJsonFilePath)));
+export const MATERIAL_CONFIG = JSON.parse(fs.readFileSync(materialJsonFilePath));
 
 export const WAVE_SETTINGS = {
     atomRadiiScale: 0.2,
