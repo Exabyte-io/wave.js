73ed661f04796ed2d06cf75ff4fe1228b46bd4f5
Mohammad Mohammadi <mohammad@exabyte.io>
2019-02-15 10:15:53 -0800

    docstring

--- tests/__tests__/mixins/atoms.js
+++ tests/__tests__/mixins/atoms.js
@@ -4,8 +4,10 @@ import {dispatchMouseDownMoveOrUpEvent, takeSnapshotAndAssertEquality} from "../
 test('addAtom', async () => {
     const wave = getWaveInstance();
     wave.toggleMouseIntersection();
-    wave["onUpdate"] = jest.fn();  // mock onUpdate
+    wave["onUpdate"] = jest.fn();  // mock onUpdate callback
+    // move the mouse to click point
     dispatchMouseDownMoveOrUpEvent(wave.container, 'mousemove', 150, 150);
+    // right click to add a new atom
     dispatchMouseDownMoveOrUpEvent(wave.container, 'contextmenu', 150, 150, 2);
     return takeSnapshotAndAssertEquality(wave.renderer.context, "addAtom");
 });
@@ -13,8 +15,10 @@ test('addAtom', async () => {
 test('removeAtom', async () => {
     const wave = getWaveInstance();
     wave.toggleMouseIntersection();
-    wave["onUpdate"] = jest.fn();  // mock onUpdate
+    wave["onUpdate"] = jest.fn();  // mock onUpdate callback
+    // move the mouse to click point (center of the canvas where there is an atom)
     dispatchMouseDownMoveOrUpEvent(wave.container, 'mousemove', WIDTH / 2, HEIGHT / 2);
+    // left click to remove the atom
     dispatchMouseDownMoveOrUpEvent(wave.container, 'mousedown', WIDTH / 2, HEIGHT / 2);
     return takeSnapshotAndAssertEquality(wave.renderer.context, "removeAtom");
 });
