0785495fd2bf0df85a9677eea70ca1d56d3e65e0
Mohammad Mohammadi <mohammad@exabyte.io>
2019-02-19 16:38:25 -0800

    Mock static files

--- tests/config.js
+++ tests/config.js
@@ -13,4 +13,10 @@ module.exports = {
 
     // A list of paths to modules that run some code to configure or set up the testing framework before each test.
     setupFilesAfterEnv: ["./setupFilesAfterEnv.js"],
+
+    // https://jestjs.io/docs/en/webpack#handling-static-assets
+    moduleNameMapper: {
+        "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/__mocks__/fileMock.js",
+        "\\.(css|less)$": "<rootDir>/__mocks__/styleMock.js"
+    }
 };
