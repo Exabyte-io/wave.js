71918a6f92d66fab7b03da7877e045ca96be980f
Mohammad Mohammadi <mohammad@exabyte.io>
2019-03-13 09:46:56 -0700

    Minor cleanups

--- run-tests.sh
+++ run-tests.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
 export NVM_DIR="/root/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
+[[ -s "$NVM_DIR/nvm.sh" ]] && . "$NVM_DIR/nvm.sh"
 
 yum install -y libXext-devel libXi-devel mesa-libGL-devel mesa-dri-drivers xorg-x11-server-Xvfb
 
 npm install
 # babel/runtime moved core-js into babel/runtime-corejs2, hence the below.
-if [ -d node_modules/@babel/runtime ]; then
+if [[ -d node_modules/@babel/runtime ]]; then
     cd node_modules/@babel/runtime
     ln -sf ../runtime-corejs2/core-js .
     cd -
