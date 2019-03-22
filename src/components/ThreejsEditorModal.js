e489c76c6eb6089145d34a8b1d5301d18ecc73c2
Mohammad Mohammadi <mohammad@exabyte.io>
2019-03-22 10:05:00 -0700

    Refactor injectScript function

--- src/components/ThreejsEditorModal.js
+++ src/components/ThreejsEditorModal.js
@@ -24,7 +24,74 @@ export class ThreejsEditorModal extends ModalDialog {
         window.localStorage.removeItem("threejs-editor");
     }
 
+    setNumberFormat() {
+        Number.prototype.format = function () {
+            return this.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1,");
+        };
+    }
+
+    /**
+     * Initialize threejs editor and add it to the dom.
+     */
+    initializeEditor() {
+
+        this.editor = new window.Editor();
+        this.editor.scene.background = new THREE.Color(settings.backgroundColor);
+
+        const viewport = new window.Viewport(this.editor);
+
+        this.domElement.appendChild(viewport.dom);
+
+        const toolbar = new window.Toolbar(this.editor);
+        this.domElement.appendChild(toolbar.dom);
+
+        const script = new window.Script(this.editor);
+        this.domElement.appendChild(script.dom);
+
+        const player = new window.Player(this.editor);
+        this.domElement.appendChild(player.dom);
+
+        const menubar = new window.Menubar(this.editor);
+        this.domElement.appendChild(menubar.dom);
+
+        const sidebar = new window.Sidebar(this.editor);
+        this.domElement.appendChild(sidebar.dom);
+
+        const modal = new window.UI.Modal();
+        this.domElement.appendChild(modal.dom);
+    }
+
+    /**
+     * Add dragover listeners to group the objects.
+     */
+    addEventListeners() {
+        const clsInstance = this;
+        document.addEventListener('dragover', function (event) {
+            event.preventDefault();
+            event.dataTransfer.dropEffect = 'copy';
+        }, false);
+
+        function onWindowResize(event) {clsInstance.editor.signals.windowResize.dispatch()}
+
+        window.addEventListener('resize', onWindowResize, false);
+        onWindowResize();
+    }
+
+    /**
+     * Load the scene based on the given materials.
+     */
+    loadScene() {
+        const loader = new THREE.ObjectLoader();
+        const scene = loader.parse(materialsToThreeDSceneData(this.props.materials));
+        this.editor.execute(new window.SetSceneCommand(scene));
+    }
+
+    /**
+     * Inject threejs editor scripts into dom.
+     * `areScriptsLoaded` flag is used to enable/disable a loader as it takes some time to load the scripts.
+     */
     injectScripts() {
+        const clsInstance = this;
         THREE_D_SOURCES.forEach(src => {
             const script = document.createElement("script");
             script.src = `${THREE_D_BASE_URL}/${src}`;
@@ -32,58 +99,11 @@ export class ThreejsEditorModal extends ModalDialog {
             script.defer = false;
             if (src.includes("SetSceneCommand")) {
                 script.onload = () => {
-
-                    this.setState({areScriptsLoaded: true});
-
-                    Number.prototype.format = function () {
-                        return this.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1,");
-                    };
-
-                    var editor = new window.Editor();
-                    editor.scene.background = new THREE.Color(settings.backgroundColor);
-
-                    var viewport = new window.Viewport(editor);
-
-                    this.domElement.appendChild(viewport.dom);
-
-                    var toolbar = new window.Toolbar(editor);
-                    this.domElement.appendChild(toolbar.dom);
-
-                    var script = new window.Script(editor);
-                    this.domElement.appendChild(script.dom);
-
-                    var player = new window.Player(editor);
-                    this.domElement.appendChild(player.dom);
-
-                    var menubar = new window.Menubar(editor);
-                    this.domElement.appendChild(menubar.dom);
-
-                    var sidebar = new window.Sidebar(editor);
-                    this.domElement.appendChild(sidebar.dom);
-
-                    var modal = new window.UI.Modal();
-                    this.domElement.appendChild(modal.dom);
-
-                    document.addEventListener('dragover', function (event) {
-                        event.preventDefault();
-                        event.dataTransfer.dropEffect = 'copy';
-                    }, false);
-
-                    document.addEventListener('drop', function (event) {
-                        if (event.dataTransfer.files.length > 0) {
-                            editor.loader.loadFile(event.dataTransfer.files[0]);
-                        }
-                    }, false);
-
-                    function onWindowResize(event) {editor.signals.windowResize.dispatch()}
-
-                    window.addEventListener('resize', onWindowResize, false);
-                    onWindowResize();
-
-                    const loader = new window.THREE.ObjectLoader();
-                    const scene = loader.parse(materialsToThreeDSceneData(this.props.materials));
-                    editor.execute(new window.SetSceneCommand(scene));
-
+                    clsInstance.setState({areScriptsLoaded: true});
+                    clsInstance.setNumberFormat();
+                    clsInstance.initializeEditor();
+                    clsInstance.addEventListeners();
+                    clsInstance.loadScene();
                 }
             }
             document.head.appendChild(script);
