c693e6dfca5153b794dc13125e5a6abf70f12a51
masahirosaito <masahiro.saito.405@ctc-g.co.jp>
2020-06-16 11:46:09 +0900

    small fix

--- src/mixins/controls.js
+++ src/mixins/controls.js
@@ -1,5 +1,5 @@
 import * as THREE from "three";
-import {CSS2DRenderer} from "three/examples/jsm/renderers/CSS2DRenderer.js"
+import {CSS2DRenderer} from "three/examples/jsm/renderers/CSS2DRenderer.js";
 import {UtilsMixin} from "./utils";
 import {addAxisLabels} from "./labels";
 
