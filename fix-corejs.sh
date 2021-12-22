33519cbb231289e6e71625d5a4518419b2ea146c
tjduigna <thomas.j.duignan@gmail.com>
2021-12-22 07:23:24 -0500

    feat: add fix-corejs.sh

---
+++ fix-corejs.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+# babel/runtime moved core-js into babel/runtime-corejs2, hence the below.
+if [[ -d node_modules/@babel/runtime ]]; then
+    cd node_modules/@babel/runtime
+    ln -sf ../runtime-corejs2/core-js .
+    cd -
+fi
