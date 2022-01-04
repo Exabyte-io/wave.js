3c6f6b852abf3664d34d797529e9fdebd578fb72
tjduigna <thomas.j.duignan@gmail.com>
2022-01-04 14:12:48 -0500

    feat: downgrade jest slightly for tests in docker

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,8 @@
 #!/usr/bin/env bash
 
-# babel/runtime moved core-js into babel/runtime-corejs2, hence the below.
-if [[ -d node_modules/@babel/runtime ]]; then
-    cd node_modules/@babel/runtime
-    ln -sf ../runtime-corejs2/core-js .
-    cd -
+if [[ "$1" == "test" ]]; then
+    JEST="npx jest --config=tests/config.js --env=jsdom"
+    xvfb-run -s "-ac -screen 0 1024x768x24" ${JEST} tests/
+else
+    npm start
 fi
-
-JEST="npx jest --config=tests/config.js --env=jsdom"
-xvfb-run -s "-ac -screen 0 1024x768x24" ${JEST} tests/
