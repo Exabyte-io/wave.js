9119eeeacde82f0907e223ddd12db80a86b8ccec
Vladislav Laktionov <vladislav.laktionov98@gmail.com>
2022-05-10 15:07:29 +0300

    feat: improved functionality of repetitions

--- src/mixins/repetition.js
+++ src/mixins/repetition.js
@@ -18,12 +18,35 @@ export const RepetitionMixin = (superclass) =>
             return Made.tools.basis.repeat(basis, repetitions).coordinates.map((c) => c.value);
         }
 
+        // eslint-disable-next-line class-methods-use-this
+        coordinatesByAxis(axis, coordinates, repetitions) {
+            if (axis === 'X')
+                return coordinates.reduce((res, item, index) => {
+                    if (item[1] === 0 && index%repetitions === 0 && res.length < repetitions) {
+                        res.push(item)
+                    }
+                    return res
+                }, [])
+            if (axis === 'Y')
+                return coordinates.reduce((res, item, index) => {
+                    if (index % repetitions === 0 && res.length < repetitions) {
+                        res.push(item)
+                    }
+                    return res
+                }, [])
+            if (axis === 'Z')
+                return coordinates.filter(item => item[0] === 0)
+            return coordinates
+        }
+
         /**
          * Repeats a given 3D object at the lattice points given by repetitionCoordinates function.
          */
         repeatObject3DAtRepetitionCoordinates(object3D) {
+            const { settings: {repetitions} } = this
+            const coordinates = this.repetitionCoordinates;
             this.structureGroup.add(object3D);
-            this.repetitionCoordinates.slice(1).forEach((point) => {
+            this.coordinatesByAxis('X', coordinates, repetitions).forEach((point) => {
                 const object3DClone = object3D.clone();
                 object3DClone.position.add(new THREE.Vector3(...point));
                 this.structureGroup.add(object3DClone);
