10f8c4166d4bb4f3367696765e0ddcafe533d3ec
Vladislav Laktionov <vladislav.laktionov98@gmail.com>
2022-05-12 17:23:20 +0300

    feat: improved all methods for axes and added method for XYZ

--- src/mixins/repetition.js
+++ src/mixins/repetition.js
@@ -20,59 +20,22 @@ export const RepetitionMixin = (superclass) =>
         // eslint-disable-next-line class-methods-use-this
         coordinatesByAxis(coordinates, repetitions) {
             const {XRepetitions, YRepetitions, ZRepetitions} = repetitions
+            const maxNumberOfRepetitions = Math.max(XRepetitions, YRepetitions, ZRepetitions)
             if (XRepetitions > 1 && YRepetitions === 1 && ZRepetitions === 1)
                 return this.getCoordinatesByXAxis(coordinates, XRepetitions)
             if (XRepetitions === 1 && YRepetitions > 1 && ZRepetitions === 1)
                 return this.getCoordinatesByYAxis(coordinates, YRepetitions)
             if (XRepetitions === 1 && YRepetitions === 1 && ZRepetitions > 1)
                 return this.getCoordinatesByZAxis(coordinates, ZRepetitions)
-            // TODO changes in these methods
-            if (XRepetitions > 1 && YRepetitions > 1 && ZRepetitions === 1) {
-                return this.getCoordinatesByXYAxes(coordinates, XRepetitions, YRepetitions)
-            }
-            if (XRepetitions > 1 && YRepetitions === 1 && ZRepetitions > 1) {
-                return this.getCoordinatesByXZAxes(coordinates, ZRepetitions)
-            }
-            if (XRepetitions === 1 && YRepetitions > 1 && ZRepetitions > 1) {
-                return this.getCoordinatesByXZAxes(coordinates, ZRepetitions)
-            }
-            // TODO method for XYZ
-            if (XRepetitions > 1 && YRepetitions > 1 && ZRepetitions > 1) {
-                return coordinates
-            }
+            if (XRepetitions > 1 && YRepetitions > 1 && ZRepetitions === 1)
+                return this.getCoordinatesByNumberOfRepetitions(this.getCoordinatesByXYAxes(coordinates, maxNumberOfRepetitions), XRepetitions, YRepetitions)
+            if (XRepetitions > 1 && YRepetitions === 1 && ZRepetitions > 1)
+                return this.getCoordinatesByNumberOfRepetitions(this.getCoordinatesByXZAxes(coordinates), XRepetitions, ZRepetitions)
+            if (XRepetitions === 1 && YRepetitions > 1 && ZRepetitions > 1)
+                return this.getCoordinatesByNumberOfRepetitions(this.getCoordinatesByYZAxes(coordinates, maxNumberOfRepetitions), YRepetitions, ZRepetitions)
+            if (XRepetitions > 1 && YRepetitions > 1 && ZRepetitions > 1)
+                return this.getCoordinatesByXYZAxes(coordinates, maxNumberOfRepetitions, XRepetitions, YRepetitions, ZRepetitions)
             return coordinates
-        }
-
-        // side
-        // eslint-disable-next-line class-methods-use-this
-        getCoordinatesByYZAxes(coordinates, repetitions) {
-            return coordinates.reduce((res, item, index) => {
-                if(index < repetitions*repetitions)
-                    res.push(item)
-                return res
-            }, [])
-        }
-
-        // down
-        // eslint-disable-next-line class-methods-use-this
-        getCoordinatesByXYAxes(coordinates, X, Y) {
-            const result = coordinates.reduce((res, item, index) => {
-                if (index % Math.max(X, Y) === 0)
-                    res.push(item)
-                return res
-            }, [])
-
-            if (X > Y)
-                return result.filter((item, index) => index%X < Y)
-            if (Y > X)
-                return result.slice(0, X*Y)
-            return result
-        }
-
-        // front
-        // eslint-disable-next-line class-methods-use-this
-        getCoordinatesByXZAxes(coordinates) {
-            return coordinates.filter(item => item[1] === 0)
         }
 
         // eslint-disable-next-line class-methods-use-this
@@ -100,6 +63,62 @@ export const RepetitionMixin = (superclass) =>
             return coordinates.filter(item => item[0] === 0)
         }
 
+        // eslint-disable-next-line class-methods-use-this
+        getCoordinatesByXYAxes(coordinates, repetitions) {
+            return coordinates.reduce((res, item, index) => {
+                if (index % repetitions === 0)
+                    res.push(item)
+                return res
+            }, [])
+        }
+
+        // eslint-disable-next-line class-methods-use-this
+        getCoordinatesByXZAxes(coordinates) {
+            return coordinates.filter(item => item[1] === 0)
+        }
+
+        // eslint-disable-next-line class-methods-use-this
+        getCoordinatesByYZAxes(coordinates, repetitions) {
+            return coordinates.reduce((res, item, index) => {
+                if(index < repetitions**2)
+                    res.push(item)
+                return res
+            }, [])
+        }
+
+        // eslint-disable-next-line class-methods-use-this
+        getCoordinatesByNumberOfRepetitions(coordinates, a, b) {
+            if (a > b)
+                return coordinates.filter((item, index) => index%a < b)
+            if (b > a)
+                return coordinates.slice(0, a*b)
+            return coordinates
+        }
+
+        // eslint-disable-next-line class-methods-use-this
+        getCoordinatesByXYZAxes(coordinates, repetitions, X, Y, Z) {
+            let columns = coordinates.reduce((res, item, index) => {
+                if (index%repetitions === 0) {
+                    res[res.length] = [item]
+                } else {
+                    res[res.length - 1].push(item)
+                }
+                return res
+            }, [])
+            if (X < repetitions)
+                columns = columns.slice(0, repetitions * X)
+            if (Y < repetitions)
+                columns = columns.filter((item, index) => index%repetitions < Y)
+            if (Z < repetitions)
+                columns = columns.map(arr => arr.filter((item, index) => index < Z))
+            return columns.reduce((res, item) => {
+                res.push(...item)
+                return res
+            }, [])
+        }
+
+
+
         /**
          * Repeats a given 3D object at the lattice points given by repetitionCoordinates function.
          */
@@ -107,7 +126,7 @@ export const RepetitionMixin = (superclass) =>
             const { settings: {XRepetitions, YRepetitions, ZRepetitions} } = this
             const coordinates = this.repetitionCoordinates(Math.max(XRepetitions, YRepetitions, ZRepetitions));
             this.structureGroup.add(object3D);
-            this.coordinatesByAxis(coordinates, {XRepetitions, YRepetitions, ZRepetitions}).forEach((point) => {
+            this.coordinatesByAxis(coordinates, {XRepetitions, YRepetitions, ZRepetitions}).slice(1).forEach((point) => {
                 const object3DClone = object3D.clone();
                 object3DClone.position.add(new THREE.Vector3(...point));
                 this.structureGroup.add(object3DClone);
