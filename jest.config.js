515a02a1bba14fcb94400a0b4c1b60130d8b9bda
tjduigna <thomas.j.duignan@gmail.com>
2022-05-16 19:10:21 -0400

    test: mock file import jest

--- jest.config.js
+++ jest.config.js
@@ -25,6 +25,7 @@ module.exports = {
 
     // mock css files: https://jestjs.io/docs/en/webpack#handling-static-assets
     moduleNameMapper: {
+        "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/tests/fileMock.js",
         "\\.(css|sass|scss)$": "<rootDir>/tests/styleMock.js"
     }
 };
