ffc7061ea90c8fc4c0b798cf4ce1ba4c289ea592
Vladislav Laktionov <vladislav.laktionov98@gmail.com>
2022-05-17 12:37:36 +0300

    refactor: updated tests

--- tests/__tests__/mixins/repetition.js
+++ tests/__tests__/mixins/repetition.js
@@ -1,5 +1,5 @@
 import { getWaveInstance } from "../../enums";
-import { takeSnapshotAndAssertEqualityAsync } from "../../utils";
+import { takeSnapshot, takeSnapshotAndAssertEqualityAsync } from "../../utils";
 
 describe('Screen tests for repeats', () => {
     const settings = {
@@ -11,30 +11,30 @@ describe('Screen tests for repeats', () => {
 
     test('Repeat along lattice vector A', () => {
         const wave = getWaveInstance({ ...settings, repetitionsAlongLatticeVectorA: 3 });
-        return takeSnapshotAndAssertEqualityAsync(wave.renderer.context, "vectorA");
+        return takeSnapshotAndAssertEqualityAsync(wave.renderer.getContext(), "vectorA");
     })
     test('Repeat along lattice vector B', () => {
         const wave = getWaveInstance({ ...settings, repetitionsAlongLatticeVectorB: 3 });
-        return takeSnapshotAndAssertEqualityAsync(wave.renderer.context, "vectorB");
+        return takeSnapshotAndAssertEqualityAsync(wave.renderer.getContext(), "vectorB");
     })
     test('Repeat along lattice vector С', () => {
         const wave = getWaveInstance({ ...settings, repetitionsAlongLatticeVectorC: 3 });
-        return takeSnapshotAndAssertEqualityAsync(wave.renderer.context, "vectorC");
+        return takeSnapshotAndAssertEqualityAsync(wave.renderer.getContext(), "vectorC");
     })
     test('Repeat along lattice vectors A and B', () => {
         const wave = getWaveInstance({ ...settings, repetitionsAlongLatticeVectorA: 3, repetitionsAlongLatticeVectorB: 3 });
-        return takeSnapshotAndAssertEqualityAsync(wave.renderer.context, "vectorAB");
+        return takeSnapshotAndAssertEqualityAsync(wave.renderer.getContext(), "vectorAB");
     })
     test('Repeat along lattice vectors A and C', () => {
         const wave = getWaveInstance({ ...settings, repetitionsAlongLatticeVectorA: 3, repetitionsAlongLatticeVectorC: 3 });
-        return takeSnapshotAndAssertEqualityAsync(wave.renderer.context, "vectorAC");
+        return takeSnapshotAndAssertEqualityAsync(wave.renderer.getContext(), "vectorAC");
     })
     test('Repeat along lattice vectors B and C', () => {
         const wave = getWaveInstance({ ...settings, repetitionsAlongLatticeVectorB: 3, repetitionsAlongLatticeVectorC: 3 });
-        return takeSnapshotAndAssertEqualityAsync(wave.renderer.context, "vectorBC");
+        return takeSnapshotAndAssertEqualityAsync(wave.renderer.getContext(), "vectorBC");
     })
     test('Repeat along lattice vectors ABC', () => {
         const wave = getWaveInstance({ ...settings, repetitionsAlongLatticeVectorA: 3, repetitionsAlongLatticeVectorB: 3, repetitionsAlongLatticeVectorC: 3 });
-        return takeSnapshotAndAssertEqualityAsync(wave.renderer.context, "vectorABC");
+        return takeSnapshotAndAssertEqualityAsync(wave.renderer.getContext(), "vectorABC");
     })
 })
