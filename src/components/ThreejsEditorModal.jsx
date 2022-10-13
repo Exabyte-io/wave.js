152a138da9b47ddcdaaabce82b8a90bceabbf07d
Timur Bazhirov <timur.bazhirov@gmail.com>
2022-10-13 16:14:28 -0700

    cleanup: whitespace

--- src/components/ThreejsEditorModal.jsx
+++ src/components/ThreejsEditorModal.jsx
@@ -105,7 +105,7 @@ export class ThreejsEditorModal extends ModalDialog {
         this.domElement.appendChild(toolbar.dom);
 
         this.initializeControls();
-        
+
         this.initializeLights();
     }
 
