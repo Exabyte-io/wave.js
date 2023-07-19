0cbb4d29da0328b09393ac94c0e9c58b66764a91
VsevolodX <79542055+VsevolodX@users.noreply.github.com>
2023-07-18 18:51:34 -0700

    update: change tests methods since rendering changed from css hide to conditional rendering

--- tests/selectors.js
+++ tests/selectors.js
@@ -2,6 +2,6 @@ export const SELECTORS = {
     viewIconToolbar: '[data-name="View"]',
     exportIconToolbar: '[data-name="Export"]',
     interactiveIconToolbar: '[data-name="Interactive"]',
-    toggleAxesRoundIconButton: 'button[aria-label="toggle axes"]',
+    toggleAxesMenuItem: '[id="toggle-axes"]',
     threeDEditorIconToolbar: '[data-name="3DEdit"]',
 };
