cf354fcbac77deab87b062023bd1b033fb0aac61
VsevolodX <vsevolodbiryukov@icloud.com>
2025-01-14 13:45:43 -0800

    update: add event listener to update wave with material

--- src/index.jsx
+++ src/index.jsx
@@ -9,5 +9,27 @@ import { ThreeDEditor } from "./components/ThreeDEditor";
 
 const domElement = document.getElementById("root");
 const material = new Made.Material(Made.defaultMaterialConfig);
+
+window.addEventListener("message", (event) => {
+    console.log("Received message from parent: " + JSON.stringify(event.data));
+    if (event.data && event.data.material) {
+        const newMaterial = new Made.Material(event.data.material);
+        if (window.threeDEditor) {
+            window.threeDEditor.setState(
+                {
+                    originalMaterial: newMaterial,
+                    material: newMaterial.clone(),
+                },
+                () => {
+                    // Force Wave component to update after state change
+                    if (window.threeDEditor.WaveComponent) {
+                        window.threeDEditor.WaveComponent.reloadViewer(true);
+                    }
+                },
+            );
+        }
+    }
+});
+
 // eslint-disable-next-line  react/no-render-return-value
 window.threeDEditor = ReactDOM.render(<ThreeDEditor editable material={material} />, domElement);
