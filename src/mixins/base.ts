a19cbf20d917382990eb34dbdc771d6f75865474
VsevolodX <vsevolodbiryukov@icloud.com>
2025-02-28 23:57:23 -0800

    update: update base  mixins

--- src/mixins/base.ts
+++ src/mixins/base.ts
@@ -14,13 +14,16 @@ export class BaseTHREEGroupManager {
 
     waveCamera: THREE.Camera;
 
-    constructor(waveStructureGroup: THREE.Group, waveCamera: THREE.Camera) {
+    wave: any;
+
+    constructor(waveStructureGroup: THREE.Group, waveCamera: THREE.Camera, wave: any) {
         this.THREEGroup = new THREE.Group();
         this.THREEGroup.name = this.THREEGroupName;
         this.THREEGroup.visible = this.isVisible;
         this.waveStructureGroup = waveStructureGroup;
         this.waveStructureGroup.add(this.THREEGroup);
         this.waveCamera = waveCamera;
+        this.wave = wave;
     }
 
     toggleVisibility() {
--- src/mixins/listeners/mixins.ts
+++ src/mixins/listeners/mixins.ts
@@ -1,58 +1,51 @@
+import * as THREE from "three";
+
+import { ATOM_GROUP_NAME } from "../../enums";
 import { listenerImplementation, raycasterImplementation } from "./implementations";
 import { IListenerCapable, IRaycasterCapable } from "./types";
 
 type Constructor<T = {}> = new (...args: any[]) => T;
 
 export const ListenersMixin = <T extends Constructor>(superclass: T) =>
-    class extends superclass implements IListenerCapable {
+    class extends superclass {
         canvas: HTMLCanvasElement;
 
-        onClick: (event: MouseEvent) => void;
-
-        onPointerMove: (event: MouseEvent) => void;
-
-        destroyListeners() {
-            listenerImplementation.destroyListeners(this);
+        onClick(event: MouseEvent) {
+            console.log("clicked");
         }
 
-        initListeners(updateState?: any, settings?: any) {
-            this.onClick = (event) => {
-                const activeMeasurementManager = this.getActiveMeasurementManager?.();
-                if (activeMeasurementManager) {
-                    activeMeasurementManager.onClick(event);
-                    this.render();
-                }
-            };
+        onPointerMove(event: MouseEvent) {
+            console.log("moved");
+        }
 
-            this.onPointerMove = (event) => {
-                const activeMeasurementManager = this.getActiveMeasurementManager?.();
-                if (activeMeasurementManager) {
-                    activeMeasurementManager.onPointerMove(event);
-                    this.render();
-                }
-            };
+        destroyListeners() {
+            this.canvas.removeEventListener("click", this.onClick);
+            this.canvas.removeEventListener("mousemove", this.onPointerMove);
+        }
 
-            listenerImplementation.initListeners(this);
+        initListeners() {
+            this.canvas.addEventListener("click", this.onClick);
+            this.canvas.addEventListener("mousemove", this.onPointerMove);
         }
     };
 
-export const RaycasterMixin = <T extends Constructor>(superclass: T) =>
-    class extends superclass implements IRaycasterCapable {
-        raycaster: THREE.Raycaster;
+export const RaycasterMixinWithListeners = <T extends Constructor>(superclass: T) =>
+    class extends ListenersMixin(superclass) {
+        raycaster: THREE.Raycaster = new THREE.Raycaster();
 
-        pointer: THREE.Vector2;
+        pointer: THREE.Vector2 = new THREE.Vector2();
 
-        camera: THREE.Camera;
+        intersectedAtom: THREE.Object3D | null = null;
 
         initRaycaster() {
-            raycasterImplementation.initRaycaster(this);
+            this.raycaster = new THREE.Raycaster();
+            this.raycaster.params.Line.threshold = 0.1;
+            this.pointer = new THREE.Vector2();
         }
 
-        checkMouseCoordinates(event: MouseEvent) {
-            raycasterImplementation.checkMouseCoordinates(this, event);
-        }
-
-        getAtomGroup(): THREE.Object3D[] {
-            return raycasterImplementation.getAtomGroup(this);
+        checkMouseCoordinates(event: MouseEvent, camera: THREE.Camera) {
+            this.pointer.x = (event.offsetX / this.canvas.width) * 2 - 1;
+            this.pointer.y = -(event.offsetY / this.canvas.height) * 2 + 1;
+            this.raycaster.setFromCamera(this.pointer, camera);
         }
     };
